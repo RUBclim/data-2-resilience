48acd575062a9d9e530475eca42a5080648281ef
Jonas Kittner <jonas.kittner@rub.de>
2025-09-17 12:34:04 +0200

    fix sitemap namespace

--- src/routes/(config-text-files)/sitemap.xml/+server.ts
+++ src/routes/(config-text-files)/sitemap.xml/+server.ts
@@ -19,7 +19,7 @@ const sitemap = (pages: string[]) => {
 <urlset
   xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
   xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
   xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
   xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
